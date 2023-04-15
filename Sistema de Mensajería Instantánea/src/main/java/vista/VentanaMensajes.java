454e27af68037b75a2b795a31163b2899185d6b5
MouGliXx <97257192+MouGliXx@users.noreply.github.com>
2023-04-15 17:43:28 -0300

    Interfaces Graficas
    Falta resolver COMO mostrar los mensajes

---
+++ Sistema de Mensajería Instantánea/src/main/java/vista/VentanaMensajes.java
@@ -0,0 +1,17 @@
+package vista;
+
+import javax.swing.*;
+
+public class VentanaMensajes {
+    private JPanel PanelPrincipal;
+    private JPanel PanelInferior;
+    private JPanel PanelSuperior;
+    private JButton EnviarButton;
+    private JLabel NombreUsuarioEmisorLabel;
+    private JTextField EscribirMensajeJTextField;
+    private JButton button1;
+    private JLabel NombreUsuarioReceptorLabel;
+    private JLabel PersonaLabel;
+    private JScrollPane ScrollPanelCentral;
+    private JTable table1;
+}
---
+++ Sistema de Mensajería Instantánea/src/main/java/vista/VentanaNotificacion.java
@@ -0,0 +1,13 @@
+package vista;
+
+import javax.swing.*;
+
+public class VentanaNotificacion {
+    private JPanel PanelPrincipal;
+    private JPanel PanelCentral;
+    private JButton button1;
+    private JButton button2;
+    private JLabel TituloLabel;
+    private JLabel Contenido1Label;
+    private JLabel Contenido2Label;
+}
