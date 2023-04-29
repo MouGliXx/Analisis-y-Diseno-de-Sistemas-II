6de7c825620b7ab30db70d426ad4d1a84e647a51
MouGliXx <97257192+MouGliXx@users.noreply.github.com>
2023-04-28 21:50:21 -0300

    fix controladorMensajes

--- Sistema de Mensajería Instantánea/src/main/java/vista/ventanas/VentanaInicio.java
+++ Sistema de Mensajería Instantánea/src/main/java/vista/ventanas/VentanaInicio.java
@@ -71,27 +71,6 @@ public class VentanaInicio extends JFrame implements IVistaInicio, ActionListene
         JOptionPane.showMessageDialog(jFrame, mensaje);
     }
 
-    public void creaOtraVentana(int tipo, String nombreUsuarioEmisor) {
-        if (tipo == 0) { //CASO VENTANA MENSAJES
-            VentanaMensajes ventanaMensajes = new VentanaMensajes();
-            ControladorMensajes controladorMensajes = new ControladorMensajes(ventanaMensajes);
-            ArrayList<IObserver> observadores = new ArrayList<>(Sistema.getInstance().getUsuario().getObservadores());
-            observadores.add(controladorMensajes);
-            Sistema.getInstance().getUsuario().setObservadores(observadores);
-            ventanaMensajes.setUsuarios(Sistema.getInstance().getUsuario().getNombreDeUsuario(), nombreUsuarioEmisor);
-            ventanaMensajes.ejecutar();
-        } else { //CASO VENTANA NOTIFICACION
-            VentanaNotificacion ventanaNotificacion = new VentanaNotificacion();
-            ControladorNotificacion controladorNotificacion = new ControladorNotificacion(ventanaNotificacion);
-            switch (tipo) {
-                case 1 -> ventanaNotificacion.setTipoVentana(1, null); //tipo 1 -> Notificacion Error
-                case 2 -> ventanaNotificacion.setTipoVentana(2, null); //tipo 2 -> Notificacion Espera
-                case 3 -> ventanaNotificacion.setTipoVentana(3, nombreUsuarioEmisor); //tipo 3 -> Notificacion Solicitud
-            }
-            ventanaNotificacion.ejecutar();
-        }
-    }
-
     @Override
     public void creaOtraVentana(Sistema sistema, int tipo, String nombreUsuarioEmisor) {
         if (tipo == 0) { //CASO VENTANA MENSAJES
