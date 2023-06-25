0f90eede5167b9eb8858afe54a1dcd5cb94b230b
ignaciosuarezquilis <ignaciogustavosuarez1234@gmail.com>
2023-06-25 11:43:52 -0300

    avances heartbeat

--- Sistema de Mensajería Instantánea/src/main/java/modelo/heartbeat/HeartApp.java
+++ Sistema de Mensajería Instantánea/src/main/java/modelo/heartbeat/HeartApp.java
@@ -18,29 +18,48 @@ public class HeartApp {
 
 
     public static void main(String[] args) throws InterruptedException, ClassNotFoundException, IOException {
-        long startTime;
-        long endTime;
+        long startTime, startTime2;
+        long endTime, endTime2;
         VentanaHeartBeat ventana = new VentanaHeartBeat("HeartBeat");
         TextField.txtPane = ventana.txtPane;
         Conexion conexion = new Conexion();
+        Conexion conexion2=new Conexion();
         while(true) {
             Socket socket = null;
+            Socket socket2 = null;
             try {
                 Thread.sleep(1000);
                 startTime = System.nanoTime();
                 socket = new Socket("localhost",1235);
+
+                startTime2 = System.nanoTime();
+                socket2 = new Socket("localhost", 1234);
+
                 conexion.setSocket(socket);
+                conexion2.setSocket(socket2);
+
                 conexion.setOutput(new ObjectOutputStream(socket.getOutputStream()));
+                conexion2.setOutput(new ObjectOutputStream(socket2.getOutputStream()));
+
                 conexion.setInput(new ObjectInputStream(socket.getInputStream()));
+                conexion2.setOutput(new ObjectOutputStream(socket2.getOutputStream()));
 
                 conexion.mandarMensaje(new Mensaje(-1,-1,"HOLA","Hola como andas","Tomas"));
 
                 endTime = System.nanoTime();
-                String ping = "\nPing: "+ (float) (endTime-startTime)/1000000 + " ms";
+                String ping = "\nPuerto 1235: "+ (float) (endTime-startTime)/1000000 + " ms";
                 TextField.txtPane.setText(TextField.txtPane.getText() + ping);
+
+                conexion2.mandarMensaje(new Mensaje(-1,-1,"HOLA","Como va","Ignacio"));
+
+
+                endTime2 = System.nanoTime();
+                String ping2  = "\nPuerto 1234: "+ (float) (endTime2-startTime2)/1000000 + " ms";
+                TextField.txtPane.setText(TextField.txtPane.getText() + ping2);
 
             } catch (IOException e) {
                 //ProcessBuilder processBuilder = new ProcessBuilder("java", "-jar", "servidor.jar");
+                System.out.println(e);
                 System.out.printf("Se cayo servidor");
                 //Process process = processBuilder.start();
                 String ubicacionActual = System.getProperty("user.dir");
