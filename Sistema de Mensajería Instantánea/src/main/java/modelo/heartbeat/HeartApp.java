45799e144c5f2dd4bb23a951f25164f7e25d5290
ignaciosuarezquilis <ignaciogustavosuarez1234@gmail.com>
2023-06-25 17:29:04 -0300

    as

--- Sistema de Mensajería Instantánea/src/main/java/modelo/heartbeat/HeartApp.java
+++ Sistema de Mensajería Instantánea/src/main/java/modelo/heartbeat/HeartApp.java
@@ -31,7 +31,7 @@ public class HeartApp {
             Socket socket = null;
             Socket socket2 = null;
             try {
-                Thread.sleep(1000);
+                Thread.sleep(5000);
                 startTime = System.nanoTime();
                 socket = new Socket("localhost",1235);
 
@@ -69,13 +69,13 @@ public class HeartApp {
                     String ping = "\nPing: " + (float) (endTime - startTime) / 1000000 + " ms";
                     TextField.txtPane.setText(TextField.txtPane.getText() + ping);
                 } catch (IOException ex){
-                    JOptionPane.showMessageDialog(null,"Se cayo el servidor 1235");
+                    JOptionPane.showMessageDialog(null,"El servidor 1234 esta caido");
                 }
 
             }
 
             try{
-                Thread.sleep(1000);
+                Thread.sleep(5000);
 
                 startTime2 = System.nanoTime();
                 socket2 = new Socket("localhost", 1234);
@@ -117,7 +117,7 @@ public class HeartApp {
                     String ping = "\nPing: " + (float) (endTime - startTime) / 1000000 + " ms";
                     TextField.txtPane.setText(TextField.txtPane.getText() + ping);
                 } catch (IOException ex){
-                    JOptionPane.showMessageDialog(null,"Se cayo el servidor 1234");
+                    JOptionPane.showMessageDialog(null,"El servidor 1234 esta caido");
                 }
             }
 
